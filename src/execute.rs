21ef170bc17ffab0ca2d8396832434e50d7ee8d2
Swoorup Joshi <swoorupjoshi@gmail.com>
2018-11-11 12:45:38 +0800

    Use std lib command spawn instead of tokio

--- src/execute.rs
+++ src/execute.rs
@@ -1,11 +1,18 @@
 use crate::types;
 
-use std::process::{Command, Stdio};
+use std::os::unix::io::*;
+use std::process::{Child, Command, Stdio};
 
+use futures::future;
+use futures::future::join_all;
+use futures::prelude::*;
 use futures::{Future, Stream};
-use tokio_process::{Child, CommandExt};
+use nix;
+use nix::unistd::Pid;
+// use tokio_process::*;
+// use tokio_process::{Child, CommandExt};
 
-pub fn spawn_cmd(cmd: &types::Cmd, stdin: Option<Stdio>, stdout: Option<Stdio>) {
+pub fn spawn_cmd(cmd: &types::Cmd, stdin: Option<Stdio>, stdout: Option<Stdio>) -> Child {
     let mut cmd_proc = Command::new(&cmd.exec);
 
     if let Some(ref args) = cmd.args {
@@ -30,25 +37,44 @@ pub fn spawn_cmd(cmd: &types::Cmd, stdin: Option<Stdio>, stdout: Option<Stdio>) 
         cmd_proc.stdout(stdout);
     }
 
-    let child = cmd_proc.spawn_async();
-
-    // Make sure our child succeeded in spawning and process the result
-    let future = child
-        .expect("failed to spawn")
-        .map(|status| println!("exit status: {}", status))
-        .map_err(|e| panic!("failed to wait for exit: {}", e));
-
-    // Send the future to the tokio runtime for execution
-    tokio::run(future)
+    // cmd_proc.spawn_async().expect(&format!("Failed to spawn process {}", cmd.exec))
+    cmd_proc
+        .spawn()
+        .expect(&format!("Failed to spawn process {}", cmd.exec))
 }
 
 pub fn interpret_config(config: &types::Config) {
+    let num_cmds = config.depends.len();
 
-    if pipe
-    let stdin = Some(Stdio::inherit());
+    let mut stdin = Some(Stdio::inherit());
 
-    for cmd in &config.depends {
-        // execute_command(&cmd, stdin.clone(), stdout.clone());
-        spawn_cmd(&cmd, None, None);
+    let mut handles = Vec::new();
+
+    for (i, cmd) in config.depends.iter().enumerate() {
+        let stdout = if i == num_cmds - 1 && config.pipe.is_some() {
+            Some(Stdio::piped())
+        } else {
+            None
+        };
+
+        let is_stdout_none = stdout.is_none();
+        let child = spawn_cmd(&cmd, stdin, stdout);
+        handles.push(child.id());
+
+        stdin = if is_stdout_none {
+            None
+        } else {
+            Some(Stdio::from(child.stdout.unwrap()))
+        };
     }
+
+    if let Some(ref cmd) = config.pipe {
+        let child = spawn_cmd(&cmd, stdin, None);
+        handles.push(child.id());
+    }
+
+    handles.iter().for_each(|id| {
+        let pid = Pid::from_raw(*id as i32);
+        nix::sys::wait::waitpid(pid, None).expect("Error awaiting process");
+    });
 }
